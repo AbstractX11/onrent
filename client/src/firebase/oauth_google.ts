2858bef7b7703ac3ba781e080af1c830efe3a4fc
Ashish Adhikari <2060aashishadhikari@gmail.com>
2021-10-19 09:32:39 +0545

    test for gitignore

--- client/src/firebase/oauth_google.ts
+++
@@ -1,6 +0,0 @@
-import { getAuth, signInWithRedirect,GoogleAuthProvider} from "firebase/auth";
-// import firebase from "firebase/compat/app";
-
-const provider = new GoogleAuthProvider();
-const auth = getAuth();
-signInWithRedirect(auth, provider);
